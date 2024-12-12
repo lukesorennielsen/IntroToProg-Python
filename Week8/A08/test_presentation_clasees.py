df9302a8999783466ae284cb0324ae7620455822
lukesorennielsen <lukesorennielsen@gmail.com>
2024-12-11 22:56:40 -0800

    workign commit

---
+++ Week8/A08/test_presentation_clasees.py
@@ -0,0 +1,6 @@
+# ------------------------------------------------------------------------------------------------- #
+# Title: test_presentation_classes
+# # Description: A collection of classes for test presentation
+# ChangeLog: (Soren, 12/11/2024, Created Script)
+# RRoot,12.1.2024,Created Script
+# ------------------------------------------------------------------------------------------------- #
---
+++ Week8/A08/test_processing_classes.py
@@ -0,0 +1,6 @@
+# ------------------------------------------------------------------------------------------------- #
+# Title: test_processing_classes
+# # Description: A collection of classes for test processing
+# ChangeLog: (Soren, 12/11/2024, Created Script)
+# RRoot,12.1.2024,Created Script
+# ------------------------------------------------------------------------------------------------- #
